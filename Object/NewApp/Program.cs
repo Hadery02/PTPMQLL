e8616866c793385df57d760ede07d0f87f806a46
Hadery02 <chuvanhiep1211@gmail.com>
2023-08-25 16:21:50 +0700

    bai tap bien so

---
+++ Object/NewApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program 
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Cac kieu du lieu trong c# : ");
+        int a = 3737823;
+            Console.WriteLine("Kieu du lieu int:  " +a );
+        long b = 98888;
+            Console.WriteLine("Kieu du lieu long:  " +b );
+        float c = -330000;
+            Console.WriteLine("Kieu du lieu float:  " +c );
+        double d = -32222211;
+            Console.WriteLine("Kieu du lieu double: " +d );
+        decimal e = -2221;
+            Console.WriteLine("Kieu du lieu decimal: " +e );
+        bool f = true;
+            Console.WriteLine("Dap an la : " +f );
+        char g = 'g';
+            Console.WriteLine("ky tu la :" +g );
+        string h = "Chu Van Hiep 2021052050";
+            Console.WriteLine("Ten va msv cua em la :" +h );
+    }
+
+
+       
+}
