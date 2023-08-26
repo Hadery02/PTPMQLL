f401cd0c919379efb632acd27567696892926c89
Hadery02 <chuvanhiep1211@gmail.com>
2023-08-26 17:33:01 +0700

    bai tap hang so

--- Object/NewApp/Program.cs
+++ Object/NewApp/Program.cs
@@ -4,21 +4,21 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.Write("Cac kieu du lieu trong c# : ");
-        int a = 3737823;
+        const int a = 3737823;
             Console.WriteLine("Kieu du lieu int:  " +a );
-        long b = 98888;
+        const long b = 98888;
             Console.WriteLine("Kieu du lieu long:  " +b );
-        float c = -330000;
+        const float c = -330000;
             Console.WriteLine("Kieu du lieu float:  " +c );
-        double d = -32222211;
+        const double d = -32222211;
             Console.WriteLine("Kieu du lieu double: " +d );
-        decimal e = -2221;
+        const decimal e = -2221;
             Console.WriteLine("Kieu du lieu decimal: " +e );
-        bool f = true;
+        const bool f = true;
             Console.WriteLine("Dap an la : " +f );
-        char g = 'g';
+        const char g = 'g';
             Console.WriteLine("ky tu la :" +g );
-        string h = "Chu Van Hiep 2021052050";
+        const string h = "Chu Van Hiep 2021052050";
             Console.WriteLine("Ten va msv cua em la :" +h );
     }
 
