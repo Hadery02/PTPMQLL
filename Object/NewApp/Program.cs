37ccbf35e258f7ecae62a237930cfce340ff1c0b
Hadery02 <chuvanhiep1211@gmail.com>
2023-08-26 19:06:29 +0700

    cau truc switch ...case

--- Object/NewApp/Program.cs
+++ Object/NewApp/Program.cs
@@ -2,23 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        int a;
-        Console.Write("Diem cua hoc sinh la : ");
-        a = Convert.ToInt32(Console.ReadLine());
-        if(a > 8){
-            System.Console.WriteLine("Hoc sinh hoc luc gioi");
-        }else if(a >=6.5){
-            System.Console.WriteLine("Hoc sinh hoc luc kha");
-        }else if(a >=5){
-            System.Console.WriteLine("Hoc sinh hoc luc trung binh ");
-        }else if(a >=3.5){
-            System.Console.WriteLine("Hoc sinh hoc luc yeu");
-        }else{
-            System.Console.WriteLine("Hoc sinh hoc luc kem");
+        int day;
+            Console.Write("Ban thich ngay nao trong tuan : ");
+            day = Convert.ToInt32(Console.ReadLine());
+        switch (day)
+        {
+            case 1:System.Console.WriteLine("Toi thich Chu nhat"); break;
+            case 2:System.Console.WriteLine("Toi thich Thu 2"); break;
+            case 3:System.Console.WriteLine("Toi thich Thu 3"); break;
+            case 4:System.Console.WriteLine("Toi thich Thu 4"); break;
+            case 5:System.Console.WriteLine("Toi thich Thu 5"); break;
+            case 6:System.Console.WriteLine("Toi thich Thu 6"); break;
+            case 7:System.Console.WriteLine("Toi thich Thu 7"); break;
+            default:
+                System.Console.WriteLine("No khong phai ngay trong tuan");
+                break;
         }
         
-    }
-
-
-       
+        
+    }   
 }
