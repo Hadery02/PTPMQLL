11daafbae466b0693ddd8abe184c243550b13fe4
Hadery02 <chuvanhiep1211@gmail.com>
2023-08-26 18:55:12 +0700

    cau truc if..else

--- Object/NewApp/Program.cs
+++ Object/NewApp/Program.cs
@@ -2,22 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Nhap vao hai so ma ban can so sanh : ");
-        int a,b;
-        System.Console.Write("a = ");
+        int a;
+        Console.Write("Diem cua hoc sinh la : ");
         a = Convert.ToInt32(Console.ReadLine());
-        System.Console.Write("b = ");
-        b = Convert.ToInt32(Console.ReadLine());
-        if (a == b) {
-            System.Console.WriteLine("Hai so bang nhau ");
-            }
-        if (a > b){
-            System.Console.WriteLine("So a lon hon so b");
-            }
-        if(a < b){        
-             System.Console.WriteLine("So a nho hon so b");
-             }
-        
+        if(a > 8){
+            System.Console.WriteLine("Hoc sinh hoc luc gioi");
+        }else if(a >=6.5){
+            System.Console.WriteLine("Hoc sinh hoc luc kha");
+        }else if(a >=5){
+            System.Console.WriteLine("Hoc sinh hoc luc trung binh ");
+        }else if(a >=3.5){
+            System.Console.WriteLine("Hoc sinh hoc luc yeu");
+        }else{
+            System.Console.WriteLine("Hoc sinh hoc luc kem");
+        }
         
     }
 
