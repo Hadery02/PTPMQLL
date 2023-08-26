a7a3212498a6e896e35ab4025fc4f9c7ccc27a29
Hadery02 <chuvanhiep1211@gmail.com>
2023-08-26 18:21:35 +0700

    toán tử so sánh

--- Object/NewApp/Program.cs
+++ Object/NewApp/Program.cs
@@ -2,16 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Nhap vao hai so ma ban can tinh : ");
+        Console.WriteLine("Nhap vao hai so ma ban can so sanh : ");
         int a,b;
         System.Console.Write("a = ");
         a = Convert.ToInt32(Console.ReadLine());
         System.Console.Write("b = ");
         b = Convert.ToInt32(Console.ReadLine());
-        System.Console.WriteLine("Hieu cua 2 so la : {0} - {1} = {2} ",a,b,a-b);
-        System.Console.WriteLine("Tich cua 2 so la : {0} * {1} = {2} ",a,b,a*b);
-        System.Console.WriteLine("Thuong cua 2 so la : {0} / {1} = {2} ",a,b,a/b);
-        System.Console.WriteLine("So du cua 2 so la : {0} % {1} = {2} ",a,b, (a - b * (a / b)));
+        if (a == b) {
+            System.Console.WriteLine("Hai so bang nhau ");
+            }
+        if (a > b){
+            System.Console.WriteLine("So a lon hon so b");
+            }
+        if(a < b){        
+             System.Console.WriteLine("So a nho hon so b");
+             }
+        
+        
     }
 
 
