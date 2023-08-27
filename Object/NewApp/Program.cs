b44602f35b828fc2dd4b338a8e0b091f5564dc7a
Hadery02 <chuvanhiep1211@gmail.com>
2023-08-27 15:24:22 +0700

    cau truc lap

--- Object/NewApp/Program.cs
+++ Object/NewApp/Program.cs
@@ -1,24 +1,35 @@
-﻿internal class Program 
+﻿public  class Program 
 {
     private static void Main(string[] args)
     {
-        int day;
-            Console.Write("Ban thich ngay nao trong tuan : ");
-            day = Convert.ToInt32(Console.ReadLine());
-        switch (day)
+        int a  = 1;
+        while (a < 10)
         {
-            case 1:System.Console.WriteLine("Toi thich Chu nhat"); break;
-            case 2:System.Console.WriteLine("Toi thich Thu 2"); break;
-            case 3:System.Console.WriteLine("Toi thich Thu 3"); break;
-            case 4:System.Console.WriteLine("Toi thich Thu 4"); break;
-            case 5:System.Console.WriteLine("Toi thich Thu 5"); break;
-            case 6:System.Console.WriteLine("Toi thich Thu 6"); break;
-            case 7:System.Console.WriteLine("Toi thich Thu 7"); break;
-            default:
-                System.Console.WriteLine("No khong phai ngay trong tuan");
-                break;
+            System.Console.WriteLine("Vòng lặp thứ {0}",a);
+            a++;
         }
-        
-        
     }   
+}
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 0;1<10;i++)
+        {
+            System.Console.WriteLine("Vong lap thu {0}",i);
+        }
+    }
+}
+
+public class Program
+{
+     private static void Main(string[] args) {
+        int a = 1;
+        do {
+            System.Console.WriteLine("Vong lap thu {0}",a);
+            a++;
+                    }
+        while(a<10);
+     }
 }
