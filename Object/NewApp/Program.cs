d793152feab325cfe1935b9f4d856fc3b843f3ad
Hadery02 <chuvanhiep1211@gmail.com>
2023-10-07 23:07:14 +0700

    PT bac 1 vs bac 2

--- Object/NewApp/Program.cs
+++ Object/NewApp/Program.cs
@@ -1,16 +1,38 @@
 ﻿using NewApp.Models;
 public class Program{
     public static void Main(string[] args)
-    {     
-        Person[] ps = new Person[3];
-    for(int i = 0; i < ps.Length; i++)
-    {
-        Person std = new Person ();
-        System.Console.Write("Nhap phan tu thu {0}: ", i);
-        int a = Convert.ToInt32(Console.ReadLine());
-        ps[i] = std;
-        System.Console.WriteLine();
-    }
+    {    
+
+        GiaiPT pt = new GiaiPT();
+        int a = 15;
+        int b = 30;
+        int c = -b/a;
+        pt.PTbac1(a,b,c);
+
+        GiaiPT pt1 = new GiaiPT();
+        int A = 10;
+        int B = -20;
+        int C = 5;
+        int delta = b*b - 4*a*c;
+        pt1.PTbac2(A,B,C,delta); 
+
+
+
+
+
+
+
+
+
+    //     Person[] ps = new Person[3];
+    // for(int i = 0; i < ps.Length; i++)
+    // {
+    //     Person std = new Person ();
+    //     System.Console.Write("Nhap phan tu thu {0}: ", i);
+    //     int a = Convert.ToInt32(Console.ReadLine());
+    //     ps[i] = std;
+    //     System.Console.WriteLine();
+    // }
 
 
 
@@ -31,22 +53,6 @@ public class Program{
 
 
 
-
-
-
-
-       /*  GiaiPT pt = new GiaiPT();
-        int a = 20;
-        int b = 40;
-        int c = -b/a;
-        pt.PTbac1(a,b,c);
-
-        GiaiPT pt1 = new GiaiPT();
-        int A = 10;
-        int B = -20;
-        int C = 5;
-        int delta = b*b - 4*a*c;
-        pt1.PTbac2(a,b,c,delta); */
      
     }
 
